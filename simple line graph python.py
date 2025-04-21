7eb240187057f0b67bbb1d48e9cf047527ed6ad9
wajihach1122 <wajihactn@gmail.com>
2025-04-21 17:50:28 +0500

    Add files via upload

---
+++ simple line graph python.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 21 17:17:57 2025
+
+@author: test
+"""
+
+import matplotlib.pyplot as plt
+x= [1,2,3,4,5]
+y = [ 1,4,6,9,7]
+plt.plot(x,y)
+plt.title('simple line plot')
+plt.xlabel('x-axix')
+plt.ylabel('y-axis')
+
+plt.show()
