8a17918ac8a7484207ab33554c01c4f11ba72dac
wajihach1122 <wajihactn@gmail.com>
2025-04-21 18:32:57 +0500

    Add files via upload

---
+++ scatter plot python.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 21 18:17:02 2025
+
+@author: test
+"""
+
+import matplotlib.pyplot as plt
+x = [1,2,3,4,5]
+y = [11,15,10,9,6]
+
+plt.scatter(x,y)
+plt.title('scatter plot')
+plt.xlabel('x-axis')
+plt.ylabel('y-axis')
+
+plt.show()
