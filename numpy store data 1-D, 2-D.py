b7fee71df868823813b8788bf27c63357a4ce45d
wajihach1122 <wajihactn@gmail.com>
2025-04-28 19:19:47 +0500

    Add files via upload

---
+++ numpy store data 1-D, 2-D.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import numpy as np 
+a = np.array([1,2,3])
+print(a) 
+b = np.array([[1,2] , [3,4]])
+print(b) 
